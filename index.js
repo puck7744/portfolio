7a79d1b61eae4d5d2ae9dad5e5363b45945633cd
Tristan Shelton <puck7744@gmail.com>
2016-11-24 23:07:57 -0500

    Advanced setup
    Preparing workflow for the rest of the project

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+//app.use(favicon('public/favicon.ico'));
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
-});
+app.get('/', function(request, response) {
+  response.render('index');
+})
 
-app.listen(app.get('port'), () => {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+app.listen(app.get('port'));
