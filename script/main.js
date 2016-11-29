e624dfa0532dccf8a4ceac8342c5de0664329bcb
Tristan Shelton <puck7744@gmail.com>
2016-11-29 09:45:49 -0500

    Second iteration of home page hero

--- script/main.js
+++ script/main.js
@@ -1,11 +1,12 @@
 require('browsernizr/test/webgl');
 var Modernizr = require('browsernizr');
-var THREE = require("three-js")();
+var THREE = require('three-js')();
 var Bird = require('./bird.js');
 var Boid = require('./boid.js');
 
 var scene, camera, renderer;
 var geometry, material, mesh;
+var bird, birds, boid, boids, clouds;
 
 function init() {
   //TODO: Update this detection to fall back on a safer method of displaying a
@@ -14,65 +15,67 @@ function init() {
 
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 450;
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 2000);
+  camera.position.z = 500;
 
   birds = [];
   boids = [];
-  birdmat = new THREE.MeshPhongMaterial( { color: 0x808080, side: THREE.DoubleSide } );
+  birdmat = new THREE.MeshBasicMaterial( { color: 0x333333, side: THREE.DoubleSide, } );
   for ( var i = 0; i < 75; i ++ ) {
-    boid = boids[ i ] = new Boid();
+    boid = boids[i] = new Boid();
+
     boid.position.x = Math.random() * 400 - 200;
     boid.position.y = Math.random() * 400 - 200;
     boid.position.z = Math.random() * 400 - 200;
+
     boid.velocity.x = 1;
     boid.velocity.y = 0;
     boid.velocity.z = 0;
+
     boid.setAvoidWalls(true);
     boid.setWorldSize(500, 500, 400);
-    bird = birds[ i ] = new THREE.Mesh( new Bird(), birdmat );
+
+    bird = birds[i] = new THREE.Mesh( new Bird(), birdmat );
     bird.phase = Math.floor( Math.random() * 62.83 );
+
     scene.add(bird);
   }
-  var verticesOfCube = [
-      -1,-1,-1,    1,-1,-1,    1, 1,-1,    -1, 1,-1,
-      -1,-1, 1,    1,-1, 1,    1, 1, 1,    -1, 1, 1,
-  ];
-  var indicesOfFaces = [
-      2,1,0,    0,3,2,
-      0,4,7,    7,3,0,
-      0,1,5,    5,4,0,
-      1,2,6,    6,5,1,
-      2,3,7,    7,6,2,
-      4,5,6,    6,7,4
-  ];
-  var cloudmat = new THREE.MeshPhongMaterial( { color: 0xeeeeee, shading: THREE.FlatShading } );
 
-  for ( var c = 0; c < 5; c ++ ) {
-    var cloud = new THREE.Group();
-    for ( var i = 0; i < 10; i ++ ) {
-      var part = new THREE.Mesh(new THREE.PolyhedronGeometry( verticesOfCube, indicesOfFaces, Math.random()*75+25, 1 ), cloudmat);
-      part.position.x = Math.random() * 400 - 200;
-      part.position.y = Math.random() * 100 - 50;
-      part.position.z = Math.random() * 200 - 100;
-      cloud.add(part);
-    }
-    cloud.position.x = Math.random() * 1000 - 500;
-    cloud.position.y = Math.random() * 500 - 250;
-    cloud.position.z = Math.random() * -500;
-    scene.add(cloud);
+  scene.fog = new THREE.Fog(0x4584b4, 200, 1500);
+
+  var texture = (new THREE.TextureLoader()).load('/img/cloud.png', null, animate);
+  texture.magFilter = THREE.LinearMipMapLinearFilter;
+  texture.minFilter = THREE.LinearMipMapLinearFilter;
+
+  var cloudmat = new THREE.MeshBasicMaterial({
+    map: texture,
+    transparent: true,
+    depthWrite: false,
+    depthTest: false
+  });
+
+  var chunk = new THREE.Geometry();
+  var plane = new THREE.Mesh(new THREE.PlaneGeometry(64, 64));
+
+  for (var i = 0; i < 1000; i++) {
+    plane.position.x = Math.random() * 2000 - 1000;
+    plane.position.y = - Math.random() * Math.random() * 100 - 100;
+    plane.position.z = -2000 + i;
+    plane.rotation.z = Math.random()*Math.sign(Math.random()-0.5);
+    plane.scale.x = plane.scale.y = Math.random() * Math.random() * 1.5 + 1.0;
+    plane.updateMatrix();
+
+    chunk.merge(plane.geometry, plane.matrix);
   }
 
-  //scene.add(new THREE.AmbientLight(0xffffff));
-  hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1 );
-	hemiLight.color.setHSL( 0.095, 0.2, 1 );
-	hemiLight.groundColor.setHSL( 0.6, 0.5, 0.8 );
-	hemiLight.position.set( 0, 500, 0 );
-	scene.add( hemiLight );
+  clouds = new THREE.Mesh(chunk, cloudmat);
+  var duplicate = new THREE.Mesh(chunk, cloudmat);
+  duplicate.position.z = 1000;
+  duplicate.updateMatrix();
+  clouds.geometry.merge(duplicate.geometry, duplicate.matrix);
+  scene.add(clouds);
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setClearColor(0xb6daff);
-	renderer.setPixelRatio(window.devicePixelRatio);
+  renderer = new THREE.WebGLRenderer({ alpha: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.getElementById('home-hero').appendChild(renderer.domElement);
@@ -81,21 +84,33 @@ function init() {
   window.addEventListener('resize', onWindowResize, false);
 }
 
+var lastTime = new Date().getTime();
 function animate() {
+  var currentTime = new Date().getTime();
+  if (currentTime-lastTime > 16) {
+    var phase = currentTime/20;
+    var phase2 = currentTime/1000;
 
-  for ( var i = 0, l = birds.length; i < l; i++ ) {
-    boid = boids[ i ];
-    boid.run( boids );
-    bird = birds[ i ];
-    bird.position.copy( boids[ i ].position );
-    // color = bird.material.color;
-    // var factor = (500-bird.position.z) / 1000;
-    // color.r = factor * 0.8; color.g = factor * 0.9; color.b = factor * 1.0;
-    bird.rotation.y = Math.atan2( - boid.velocity.z, boid.velocity.x );
-    bird.rotation.z = Math.asin( boid.velocity.y / boid.velocity.length() );
-    bird.phase = ( bird.phase + ( Math.max( 0, bird.rotation.z ) + 0.1 )  ) % 62.83;
-    bird.geometry.vertices[ 5 ].y = bird.geometry.vertices[ 4 ].y = Math.sin( bird.phase ) * 5;
+    for (var i = 0, l = birds.length; i < l; i++) {
+      boid = boids[i];
+      boid.run(boids);
+
+      bird = birds[i];
+      bird.position.copy(boids[i].position);
+
+      bird.rotation.y = Math.atan2( - boid.velocity.z, boid.velocity.x );
+      bird.rotation.z = Math.asin( boid.velocity.y / boid.velocity.length() );
+
+      if (boid.velocity.y < 0) bird.geometry.vertices[5].y = bird.geometry.vertices[4].y = 0;
+      else bird.geometry.vertices[5].y = bird.geometry.vertices[4].y = Math.sin(bird.phase+phase) * 5;
+      // bird.geometry.vertices[5].y = bird.geometry.vertices[4].y = Math.sin(bird.phase+phase)*Math.max(0, Math.sign(Math.sin(bird.phase+phase2))) * 5;
+      bird.geometry.verticesNeedUpdate = true;
+    }
+
+    lastTime = currentTime;
   }
+
+  clouds.position.z = 500 + currentTime*0.03 % 1000;
 
   renderer.render(scene, camera);
 
@@ -104,10 +119,10 @@ function animate() {
 
 function onDocumentMouseMove( event ) {
   var vector = new THREE.Vector3( event.clientX - (window.innerWidth / 2), - event.clientY + (window.innerHeight / 2), 0 );
-  for ( var i = 0, il = boids.length; i < il; i++ ) {
-    boid = boids[ i ];
+  for (var i = 0, l = boids.length; i < l; i++) {
+    boid = boids[i];
     vector.z = boid.position.z;
-    boid.repulse( vector );
+    boid.repulse(vector);
   }
 }
 
